dd719ec4ca78b6a8032e60b746c70ce6ba48de5e
Daniel Raymond <dan@raymond.ch>
2024-06-07 22:09:37 -0400

    Added desmos graph

---
+++ src/routes/Graph.svelte
@@ -0,0 +1,25 @@
+<script lang="js">
+	// @ts-ignore
+	import Desmos from 'desmos';
+    import { onMount } from 'svelte';
+
+	const elt = document.createElement('div');
+	elt.style.width = '600px';
+	elt.style.height = '400px';
+
+	const calculator = Desmos.GraphingCalculator(elt, {keypad: false, expressions: false, settingsMenu: false});
+	calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
+
+    onMount(() => {
+        const container = document.getElementById('calculator');
+        if (container)
+            container.appendChild(elt);
+        else
+            console.error('Unable to mount Graph');
+    });
+</script>
+
+<div id="calculator">
+	<!-- Calculator Goes here -->
+</div>
+
